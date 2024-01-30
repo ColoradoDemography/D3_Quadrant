7b16f1fb1486137e77362c26fafe14ff99165444
Bleess <TBleess@dola.state.co.us>
2024-01-30 16:25:05 -0700

    18-22

--- js/app.js
+++ js/app.js
@@ -191,7 +191,7 @@ function stylesubmit(){
     var yval=$("#selecty").val();
     var zval=$("#selectz").val();  
          
-    var db='acs1519';
+    var db='acs1822';
     var schema='data';
     var limit='10000';
       
@@ -201,8 +201,8 @@ function stylesubmit(){
     var geog=$("#selectgeo").val(); 
       
       if(geog=='cc'){sumlev="50"; state="8";}
-      //if(geog=='cp'){sumlev="160";}
-      if(geog=='st'){sumlev="40"; state="";}
+      if(geog=='cp'){sumlev="160"; state="8";}
+      if(geog=='st'){sumlev="40"; state="1,2,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,44,45,46,47,48,49,50,51,53,54,55,56";}
 
       
       //var state="8";
